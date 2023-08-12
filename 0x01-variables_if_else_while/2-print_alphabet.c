6348a58d0a6b910255101d5a1329ca2e3d52a8e6
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-12 05:00:06 -0700

    This program prints out alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alphabet = 'a'; /* starts with letter a */
+
+	while (alphabet <= 'z')
+
+	{
+		printf("%c ", alphabet);
+		alphabet++; /* keeps adding new alphabets until 'z' */
+	}
+	printf("\n");
+	return (0);
+}
