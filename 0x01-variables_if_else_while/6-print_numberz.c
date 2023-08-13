b200724651e97e310ddfc3885e4f4d3309957946
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-13 08:08:28 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		char sd_char = '0' + n;
+
+		putchar(sd_char);
+	}
+	putchar('\n');
+	return (0);
+}
