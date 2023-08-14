1500937af4fcd22768c3afa858c8c2836725f9d2
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-14 08:37:29 -0700

    Count in double digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int m;
+	int n;
+
+	for (m = 0; m < 10; ++m)
+	{
+		for (n = m + 1; n < 10; ++n)
+		{
+			putchar(m + '0');
+			putchar(n + '0');
+			if (m != 8 || n != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
