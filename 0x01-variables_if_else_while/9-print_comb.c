00cf8a091bd4372bce81801b3828f69d50730632
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-13 17:09:25 -0700

    Combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,13 @@ int main(void)
 
 	for (n = 0; n < 10; n++)
 	{
-		putchar(n + '0');
-		putchar(',');
-		putchar(' ');
+		if (n == 9)
+			putchar(n + '0');
+		else
+		{	putchar(n + '0');
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
