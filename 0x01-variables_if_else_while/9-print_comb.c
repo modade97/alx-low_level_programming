c8ab390d13dde8ad646e25162a0fed1c56ff210b
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-13 17:00:44 -0700

    All possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	putchar('0');
+	int n;
 
-	for (int n = 1; n <= 9; n++)
+	for (n = 0; n < 10; n++)
 	{
+		putchar(n + '0');
 		putchar(',');
 		putchar(' ');
-		putchar(n + '0');
 	}
 	putchar('\n');
 	return (0);
