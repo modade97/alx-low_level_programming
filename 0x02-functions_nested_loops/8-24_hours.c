c8cc5a2ceeb8f55f6d36ba92692ae428134e10d4
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-16 18:16:10 -0700

    Jack Bauer countdown Clock

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@ void jack_bauer(void)
 {
 	int m, n;
 
-	for (m = 0; m < 60; m++)
+	for (m = 0; m < 24; m++)
 	{
-		for (n = 0; n < 24; n++)
+		for (n = 0; n < 60; n++)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
-			_putchar(':');
 			_putchar(m / 10 + '0');
 			_putchar(m % 10 + '0');
+			_putchar(':');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
 			_putchar('\n');
 		}
 	}
