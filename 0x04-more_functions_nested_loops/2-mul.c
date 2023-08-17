575db5ec3cb536dd21d9ae99d8ad16e120b6919a
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-17 07:51:48 -0700

    Multiply 2 integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - function multiplies a with b parameters
+ * @a: function parameter
+ * @b: function parameter 2
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
