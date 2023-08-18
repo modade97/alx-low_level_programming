d100a5c268e815998e922029a422683887d1e154
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-18 01:41:41 -0700

    Size of squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - function prints out #
+ * @size: function parameter
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
