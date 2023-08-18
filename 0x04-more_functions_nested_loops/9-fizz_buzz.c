5d093109d57329db46bc6f632ba62ec0ab54d78e
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-18 02:46:51 -0700

    FizzBuzz, Fizz, Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s ", "FizzBuzz");
+			printf("%s", "FizzBuzz");
 		else if (i % 3 == 0)
-			printf("%s ", "Fizz");
+			printf("%s", "Fizz");
 		else if (i % 5 == 0)
-			printf("%s ", "Buzz");
+			printf("%s", "Buzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
