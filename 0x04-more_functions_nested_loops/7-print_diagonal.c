1878d7f18688981ca789f02d40e5e26ec2e7d67d
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-17 17:12:29 -0700

    Backslash and sapce

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - function prints backslash based on 'n'
+ * @n: function parameter
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
