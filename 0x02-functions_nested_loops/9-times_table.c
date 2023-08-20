ea5f44b03a20156812af6298fd3c2ff471604f04
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-20 07:45:57 -0700

    Prime factor

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * times_table - function, prints out the 9 times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	char m, n, result;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 0; n < 0; n++)
+		{
+			result = m * n;
+			_putchar("%d, ", result);
+		}
+	}
+}
