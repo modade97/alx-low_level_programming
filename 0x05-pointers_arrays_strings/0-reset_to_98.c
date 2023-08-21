c6d5e9eb7aaeb30ed172f73eec799c0a1d696010
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-21 06:11:30 -0700

    Dereferencing with pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - function resets int to 98
+ * @n: function parameter. ALso a pointer
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
