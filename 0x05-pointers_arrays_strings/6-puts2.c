03e1181e67fd9936db38088a7685dbc8824a4519
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-21 17:19:44 -0700

    Prints out every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - main function
+ * @str: function parameter also a pointer
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
