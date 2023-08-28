2962f4052dee73c9deb49357457d42c4f1e7bcfd
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-08-28 05:46:33 -0700

    Memory copy function

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - memory copy function
+ * @dest: destination pointer parameter
+ * @src: source pointer parameter
+ * @n: unsigned integer parameter
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*dest = src[i];
+	}
+	return (dest);
+}
