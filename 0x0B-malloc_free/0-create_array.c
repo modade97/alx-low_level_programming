0c48df8a40055fa57add1f341feac8dc7f108dda
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-09-04 10:33:39 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_array - Pointer function creates arrays
+ * @size: function parameter
+ * @c: also function parameter
+ * Return: arr
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
