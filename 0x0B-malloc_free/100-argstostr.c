d3e08be49dab048d10946adfb8c69af344011f1c
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-09-04 17:46:14 -0700

    Concatenate arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * argstostr - Function concatenates all argyments in program
+ * @ac: function parameter
+ * @av: function parameter
+ * Return: NULL if fail | else pointer to a new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int j;
+	int k = 0;
+	int count = 0;
+	char *rtm;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			count++;
+		count++;
+	}
+	rtm = malloc(sizeof(char) * count + 1);
+	if (rtm == NULL)
+		return (NULL);
+	
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			rtm[k] = av[i][j];
+			k++;
+		}
+		rtm[k++] = '\n';
+	}
+	rtm[k] = '\0';
+	return (rtm);
+}
