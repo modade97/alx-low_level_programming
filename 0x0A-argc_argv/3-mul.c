ce4395abb479ad2ed2eda69a8b03b2ceff589e60
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-09-06 14:17:38 -0700

    Multiplies two(2) numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program multiplies arguments received
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: if argc < 3 (1) else (0);
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int mul = 0;
+
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+
+		mul = num1 * num2;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
