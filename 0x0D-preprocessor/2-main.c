296734f214c3546722f9c03c9a8f96a9cb08a7ce
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-09-12 04:23:35 -0700

    prints file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function to print out the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main (void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
