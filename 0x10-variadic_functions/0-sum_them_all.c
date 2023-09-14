deb4560b05e8fa769c74d33e2c742414c4b65321
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-09-14 07:41:05 -0700

    Variadic Sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function adds all parameters and returns it
+ * @n: const parameter
+ * @...: variadic declaration
+ * Return: sum of variadic arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int result = 0;
+	unsigned int i;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		result = result + va_arg(args, int);
+
+	va_end(args);
+	return (result);
+}
