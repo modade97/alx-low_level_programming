26a5a4544d3c1b864a81a06e9d995d0380a935ba
Modadeoluwa Adedamola <modadegospel@gmail.com>
2023-09-14 21:45:37 -0700

    print num and new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,15 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
-		if (separator != NULL)
-		{
-			printf("%d", va_arg(args, int));
+		printf("%d", va_arg(args, int));
+
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
-		}
-		else
-			printf("%d", va_arg(args, int));
 	}
 	printf("\n");
 	va_end(args);
